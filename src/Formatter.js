bc141deda916e66e9c8a7dc6b604d675a8de5764
光弘 <guanghong.wsj@alibaba-inc.com>
2016-07-22 13:09:42 +0800

    fix issue #4

--- src/Formatter.js
+++ src/Formatter.js
@@ -10,26 +10,38 @@ var Formatter = {};
 
 Formatter.date = function(str, pattern) {
     var date = new Date(str);
-    pattern = pattern || 'YYYY-MM-DD';
-    var o = {
-        "M+": date.getMonth() + 1, //月份 
-        "D+": date.getDate(), //日 
-        "d+": date.getDate(), //日 
-        "H+": date.getHours(), //小时 
-        "h+": date.getHours(), //小时 
-        "m+": date.getMinutes(), //分 
-        "s+": date.getSeconds(), //秒 
-        "Q+": Math.floor((date.getMonth() + 3) / 3), //季度 
-        "q+": Math.floor((date.getMonth() + 3) / 3), //季度 
-        "S": date.getMilliseconds() //毫秒 
-    };
-    if (/(y+)/i.test(pattern)) {
-        pattern = pattern.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    if ( Object.prototype.toString.call(date) === "[object Date]" ) {
+        // it is date
+
+        if (isNaN(date.getTime())) {
+            // invalid
+            console.warn('Formatter: invalid date');
+            return "";
+        }
+        pattern = pattern || 'YYYY-MM-DD';
+        var o = {
+            "M+": date.getMonth() + 1, //月份 
+            "D+": date.getDate(), //日 
+            "d+": date.getDate(), //日 
+            "H+": date.getHours(), //小时 
+            "h+": date.getHours(), //小时 
+            "m+": date.getMinutes(), //分 
+            "s+": date.getSeconds(), //秒 
+            "Q+": Math.floor((date.getMonth() + 3) / 3), //季度 
+            "q+": Math.floor((date.getMonth() + 3) / 3), //季度 
+            "S": date.getMilliseconds() //毫秒 
+        };
+        if (/(y+)/i.test(pattern)) {
+            pattern = pattern.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+        }
+        for (var k in o) {
+            if (new RegExp("(" + k + ")").test(pattern)) pattern = pattern.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+        return pattern;
     }
-    for (var k in o) {
-        if (new RegExp("(" + k + ")").test(pattern)) pattern = pattern.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    }
-    return pattern;
+    console.warn('Formatter: invalid date');
+    return "";
+    
 }
 
 Formatter.money = function(str, delimiter, fixedNum) {
